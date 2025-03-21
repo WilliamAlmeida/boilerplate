72e004ab79d6df0c541471de6ed26847c50c0061
William-Almeida <williamkillerca@hotmail.com>
2025-03-21 10:19:59 -0300

    Add create and edit Livewire components for financiamentos management

--- resources/views/livewire/admin/financiamentos/financiamentos-index.blade.php
+++ resources/views/livewire/admin/financiamentos/financiamentos-index.blade.php
@@ -7,7 +7,7 @@
         <x-slot:actions>
             <x-button label="Filtros" @click="$wire.drawer = true" responsive icon="o-funnel" class="btn-primary" />
             @if($can->create)
-                {{-- <x-button label="Novo" wire:click="$dispatch('create')" responsive icon="o-plus" class="btn-primary" /> --}}
+                <x-button label="Novo" wire:click="$dispatch('create')" responsive icon="o-plus" class="btn-primary" />
             @endif
         </x-slot:actions>
     </x-header>
@@ -35,14 +35,34 @@
     <x-drawer wire:model="drawer" title="Filtros" right separator with-close-button class="lg:w-1/3">
         <div class="flex flex-col space-y-2">
             <x-input placeholder="Search..." wire:model.live.debounce="search" icon="o-magnifying-glass" @keydown.enter="$wire.drawer = false" />
+
+            <x-choices label="Cliente" wire:model="filter.cliente" :options="$clientesSearchable" search-function="searchClients" optionLabel="nome_fantasia" placeholder="Selecione uma opção" single searchable />
+
+            <div class="grid grid-cols-2 gap-4">
+                <x-input label="Banco/Perfil" wire:model="filter.banco_perfil" clearable />
+                <x-input label="Status" wire:model="filter.status" clearable />
+            </div>
+
+            <div class="grid grid-cols-2 gap-4">
+                <x-input label="Vendedor" wire:model="filter.vendedor" clearable />
+                <x-input label="CPF" wire:model="filter.cpf" x-mask="999.999.999-99" clearable />
+            </div>
+
+            <div class="flex flex-col">
+                <x-mary.label label="Data" class="font-semibold" />
+                <div class="grid grid-cols-2 gap-4">
+                    <x-input label="Início" wire:model="filter.data_i" type="date" />
+                    <x-input label="Término" wire:model="filter.data_e" type="date" />
+                </div>
+            </div>
         </div>
 
         <x-slot:actions>
             <x-button label="Resetar" icon="o-x-mark" wire:click="clear" spinner />
-            <x-button label="Feito" icon="o-check" class="btn-primary" @click="$wire.drawer = false" />
+            <x-button label="Feito" icon="o-check" class="btn-primary" @click="$dispatch('table:refresh'); $wire.drawer = false" />
         </x-slot:actions>
     </x-drawer>
 
-    {{-- <livewire:admin.financiamentos.financiamentos-create /> --}}
-    {{-- <livewire:admin.financiamentos.financiamentos-edit /> --}}
+    <livewire:admin.financiamentos.financiamentos-create />
+    <livewire:admin.financiamentos.financiamentos-edit />
 </div>
