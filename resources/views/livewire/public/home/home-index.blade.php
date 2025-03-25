b283e41d5197597523ce8ff8e751f094a36d5dc5
William-Almeida <williamkillerca@hotmail.com>
2025-03-25 20:26:32 -0300

    Add contratos-stats component to home index for authenticated users

--- resources/views/livewire/public/home/home-index.blade.php
+++ resources/views/livewire/public/home/home-index.blade.php
@@ -1,4 +1,8 @@
 <div>
     <!-- HEADER -->
     <x-header title="{{ config('app.name') }}" separator progress-indicator />
+
+    @auth
+        <livewire:components.statistics.contratos-stats />
+    @endauth
 </div>
