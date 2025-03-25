d592d60bb9d30dd29eec5d796d84cc6c61f4625b
William-Almeida <williamkillerca@hotmail.com>
2025-03-25 20:24:46 -0300

    Add foreign key reference for user_id in vendedores table

--- database/migrations/2025_03_21_102855_create_vendedores_table.php
+++ database/migrations/2025_03_21_102855_create_vendedores_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('vendedores', function (Blueprint $table) {
             $table->id();
             $table->string('nome', 100);
+            $table->foreignIdFor(\App\Models\User::class, 'user_id')->nullable()->cascadeOnUpdate()->nullOnDelete();
             $table->timestamps();
             $table->softDeletes();
         });
