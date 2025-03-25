ae92f9c02d8473e6e7ba406bb7a70211896c4ec2
William-Almeida <williamkillerca@hotmail.com>
2025-03-25 20:40:15 -0300

    Update role check to include super admin for vendedor selection in contratos-stats component

--- resources/views/livewire/components/statistics/contratos-stats.blade.php
+++ resources/views/livewire/components/statistics/contratos-stats.blade.php
@@ -1,7 +1,7 @@
 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
 
     <div class="col-span-full">
-        @hasanyrole('admin')
+        @hasanyrole('admin|super admin')
         <x-choices-offline label="Vendedor" wire:model.live.debounce="vendedor_id" :options="$arr_vendedores" optionLabel="nome" placeholder="Filtra por vendedor" 
             single />
         @endhasanyrole
