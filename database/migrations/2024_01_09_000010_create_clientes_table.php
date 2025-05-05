722add445a1bdefb17229612b95486c15d87f1c9
William-Almeida <williamkillerca@hotmail.com>
2025-05-05 15:10:10 -0300

    Refactor clientes migration: update schema to use UUID and add new fields; remove clientes_numeros and clientes_emails migrations

--- database/migrations/2024_01_09_000010_create_clientes_table.php
+++ database/migrations/2024_01_09_000010_create_clientes_table.php
@@ -14,17 +14,16 @@ return new class extends Migration
     public function up()
     {
         Schema::create('clientes', function (Blueprint $table) {
-            $table->id();
-            $table->string('tipo', 45);
-            $table->string('nome_fantasia', 200);
-            $table->string('cpf', 45)->nullable();
-            $table->string('cnpj', 45)->nullable();
-            $table->string('razao', 200);
-            $table->foreignIdFor(\App\Models\Estados::class, 'estado_id')->nullable()->cascadeOnUpdate()->nullOnDelete();
-            $table->foreignIdFor(\App\Models\Cidades::class, 'cidade_id')->nullable()->cascadeOnUpdate()->nullOnDelete();
-            $table->string('cep', 45)->nullable();
-            $table->string('endereco', 200)->nullable();
-            $table->string('bairro', 200)->nullable();
+            $table->uuid('id')->primary();
+            $table->text('nome')->nullable();
+            $table->timestamp('data_cadastro')->nullable()->useCurrent();
+            $table->json('tags_personalidade')->nullable();
+            $table->date('data_nascimento')->nullable();
+            $table->text('email')->nullable();
+            $table->string('phone_1', 20)->nullable();
+            $table->string('phone_2', 20)->nullable();
+            $table->string('phone_3', 20)->nullable();
+
             $table->timestamps();
             $table->softDeletes();
         });
